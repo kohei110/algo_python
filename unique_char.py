14349b38f5ebeae4afa7390c0dabbbd922e5ce3c
kohei110 <kohei.nishitani@gmail.com>
2023-04-09 23:31:13 -0700

    new q

--- unique_char.py
+++ unique_char.py
@@ -17,3 +17,21 @@ def solution(s):
 print(solution('alphabet'))
 print(solution('barbados'))
 print(solution('crunchy'))
+
+
+#Approach 2
+import collections
+
+def solution(s):
+    # build hash map : character and how often it appears
+    count = collections.Counter(s) # <-- gives back a dictionary with words occurrence count 
+                                         #Counter({'l': 1, 'e': 3, 't': 1, 'c': 1, 'o': 1, 'd': 1})
+    # find the index
+    for idx, ch in enumerate(s):
+        if count[ch] == 1:
+            return idx     
+    return -1
+
+print(solution('alphabet'))
+print(solution('barbados'))
+print(solution('crunchy'))
